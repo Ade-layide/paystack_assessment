dc0f1f2bfa64d3f89a9c6dee8df3e5701dc80c75
Olayide Ademeso <OAdemeso@renlagos.com>
2022-02-22 02:09:24 +0100

    test folder added

---
+++ tests/1_verification_test.js
@@ -0,0 +1,24 @@
+const request = require("supertest")("https://swapi.dev/api");
+var assert = require('assert');
+var expect = require("chai").expect;
+
+
+describe('GET /planets/3', function() {
+
+    it('Verifiies response headers', () {
+
+    })
+
+    it('Verifiies the response data', () {
+        
+    })
+
+    it('Verifiies the json schema', () {
+        
+    })
+
+    it('Verifiies the response time is less than 3ms', () {
+        
+    })
+
+})
