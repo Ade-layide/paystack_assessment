7310eec8a8eede81408ca4aa9807574bb07b9570
Olayide Ademeso <OAdemeso@renlagos.com>
2022-02-23 16:55:07 +0100

    Test to verify JSON schema ADDED

--- tests/verifyPlanet.js
+++ tests/verifyPlanet.js
@@ -1,8 +1,11 @@
 const request = require("supertest")("https://swapi.dev/api");
 const assert = require('assert');
+const chai = require('chai');
 const expect = require("chai").expect;
 const planet = require("../data/planet");
+const schema = require("../data/schema");
 
+chai.use(require('chai-json-schema'));
 
 
 describe('GET /planets/3', function() {
@@ -12,20 +15,20 @@ describe('GET /planets/3', function() {
     });
 
     it('Verifiies response headers', () => {
-        
+   
         expect(response.statusCode).to.equal(200);
 
         expect(response.header["content-type"]).to.include("application/json");
         
         expect(response.header["vary"]).to.contain("Accept", "Cookie");
-
+       
     })
 
     it('Verifiies the response data', () => {
-       //verify that a response body is returned
-        expect(response.body).exist
+       //verify that a response body is returned and is an object
+        expect(response.body).exist;
 
-       //Verifies the response data using expected data stored in planetjs file
+       //Verifies the response data using expected data in planet file
         assert.equal(response.body.name, planet.planets.name);
         assert.equal(response.body.rotation_period, planet.planets.rotation_period);
         assert.equal(response.body.orbital_period, planet.planets.orbital_period);
@@ -35,16 +38,16 @@ describe('GET /planets/3', function() {
         assert.equal(response.body.terrain, planet.planets.terrain);
         assert.equal(response.body.surface_water, planet.planets.surface_water);
         assert.equal(response.body.population, planet.planets.population);
-
-        
+      
     })
 
-  //  it('Verifiies the json schema', () {
-        
-  //  })
+    it('Verifiies the json schema', () => {
+        expect(response.body).exist
+        expect(response.body).to.be.jsonSchema(schema.planetSchema)
+    })
 
-   // it('Verifiies the response time is less than 3ms', () {
+    it('Verifiies the response time is less than 3ms', () => {
         
-  //  })
+    })
 
 })
